34cecde61a69f073edea426287336499db1fda5d
unknown <kspsagar01@gmail.com>
2024-09-25 17:59:32 +0530

    dynamic data updation using postman

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const ConnectDB = async ()=>{
-    mongoose.connect("mongodb+srv://kspsagar02:RIpstyxSs8khDRsu@database.ocbai.mongodb.net/");
-};
+const connectDB = async ()=>{
+    mongoose.connect("mongodb+srv://kspsagar02:RIpstyxSs8khDRsu@database.ocbai.mongodb.net/")
+}
 
-module.exports = ConnectDB;
+module.exports = connectDB;
